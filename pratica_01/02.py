4ebbaa0fb05141cfb9c8524a311265678d664c7a
Adriano Ramos <adrianorms20@gmail.com>
2026-01-11 09:48:51 -0300

    Primeiro commit

---
+++ pratica_01/02.py
@@ -0,0 +1,9 @@
+"""
+2- Calculadora de Soma
+* Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.
+"""
+
+num1 = 12
+num2 = 14
+
+print(num1+num2)
---
+++ pratica_01/04.py
@@ -0,0 +1,16 @@
+"""
+4- Calculadora de Preço Total
+* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+* Nome do produto: "Cadeira Infantil"
+* Preço unitário: R$ 12.40
+* Quantidade: 3
+O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+"""
+
+print('Nome do produto: "Cadeira Infantil"')
+print('Preço unitário: R$ 12.40')
+print('Quantidade: 3')
+precoUnit = 12.4
+qtde = 3
+print('Preço total: ', precoUnit*qtde)
